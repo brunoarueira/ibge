7c659d983f7b75ecd26480e85201d44f66439988
Bruno Arueira <contato@brunoarueira.com>
2015-06-28 19:38:07 -0300

    Change source from state reader to sidra

--- lib/ibge/reader/state.rb
+++ lib/ibge/reader/state.rb
@@ -1,8 +1,11 @@
 module Ibge
   module Reader
     class State < Base
+      source :sidra
+
       columns "0": :code,
-              "1": :state_name
+              "1": :state_name,
+              "2": :state_federative_unit
     end
   end
 end
